1be7947b234a580db393ac222b006764b86aa766
donghyun.ethan.kim <donghyun.ethan.kim@gmail.com>
2022-08-14 15:18:24 -0400

    Updating frontend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 import {ForceGraph2D} from 'react-force-graph';
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
+
 import graphData from './Components/graphcopy.json';
 import data from './Components/miserables.json'
 function App() {
@@ -60,6 +69,9 @@ function App() {
 
             setLinks(array => [...array, linkObj])
         }
+
+        //setgData((array) => array.filter(tempData => tempData.id !== "@id1"));
+        //setgData((array) => array.filter(tempData => tempData.id !== "@id2"));
         
     })
   }
@@ -72,37 +84,45 @@ function App() {
 
   return (
       // <div> {gdata[gdata.length - 1]["sentiment"]} </div>
-
       <div>
-        <ForceGraph2D
-          graphData={{ nodes: gdata, links: links }}
-          nodeAutoColorBy="sentiment"
-          nodeCanvasObject={(node, ctx, globalScale) => {
-            const label = node.id;
-            const fontSize = 12/globalScale;
-            ctx.font = `${fontSize}px Sans-Serif`;
-            const textWidth = ctx.measureText(label).width;
-            const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+          <Typography variant="h1" style = {{'margin-left': '30px', 'margin-top' : '20px'}}> Tweet Sentiment Visualizer </Typography>
+          <Typography variant="h6" style = {{'margin-left': '40px', 'margin-top' : '20px'}}> Enter a topic to analyze! </Typography>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" style = {{'margin-left': '40px', 'margin-top' : '20px'}}/>
 
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-            ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+          <div style={{
+                  position: 'absolute',
+                  bottom: 0,
+              }}>
+            <ForceGraph2D
+              graphData={{ nodes: gdata, links: links }}
+              height = {600}
+              nodeAutoColorBy="sentiment"
+              nodeCanvasObject={(node, ctx, globalScale) => {
+                const label = node.id;
+                const fontSize = 12/globalScale;
+                ctx.font = `${fontSize}px Sans-Serif`;
+                const textWidth = ctx.measureText(label).width;
+                const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
 
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillStyle = node.color;
-            ctx.fillText(label, node.x, node.y);
+                ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
 
-            node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-          }}
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillStyle = node.color;
+                ctx.fillText(label, node.x, node.y);
 
-          nodePointerAreaPaint={(node, color, ctx) => {
-            ctx.fillStyle = color;
-            const bckgDimensions = node.__bckgDimensions;
-            bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
-          }}
-        />
+                node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+              }}
+
+              nodePointerAreaPaint={(node, color, ctx) => {
+                ctx.fillStyle = color;
+                const bckgDimensions = node.__bckgDimensions;
+                bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+              }}
+            />
+          </div>
       </div>
-
 
     /*<div className="App">
       <header className="App-header">
